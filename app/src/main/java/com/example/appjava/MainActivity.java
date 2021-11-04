4f133662a112d25cfc7d4be7928f798ffa7bf233
Maulana Arif Wijaya <88649781+LeToyek@users.noreply.github.com>
2021-11-04 21:22:10 +0700

    siap dikumpulkan

--- app/src/main/java/com/example/appjava/MainActivity.java
+++ app/src/main/java/com/example/appjava/MainActivity.java
@@ -3,28 +3,25 @@ package com.example.appjava;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView tvNama;
+    EditText edtNama;
+    Button input;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvNama = findViewById(R.id.tvNama);
+        edtNama = findViewById(R.id.edtNama);
+        input = findViewById(R.id.btnTampil);
 
     }
     public void submit(View view){
-        TextView firstName = (findViewById(R.id.textFirstName));
-        TextView lastName = (findViewById(R.id.textLastName));
-        TextView email = (findViewById(R.id.textEmail));
-
-        EditText editFirstName = findViewById(R.id.editFirstName);
-        EditText editLastName = findViewById(R.id.editLastName);
-        EditText editEmail = findViewById(R.id.editEmail);
-
-        firstName.setText(editFirstName.getText().toString());
-        lastName.setText(editLastName.getText().toString());
-        email.setText(editEmail.getText().toString());
+        tvNama.setText(edtNama.getText().toString());
     }
 }
